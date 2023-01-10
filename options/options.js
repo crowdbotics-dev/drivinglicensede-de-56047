a62143fcbc3a0f989df84a56e481814dba2ff5b9
Crowdbotics <engineering@crowdbotics.com>
2023-01-10 19:37:40 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "drivinglicensede-de-56047",
+  url: "https://drivinglicensede-de-56047.botics.co",
+  api: "https://drivinglicensede-de-56047.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
